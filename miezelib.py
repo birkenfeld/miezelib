Several fixes to error calculation, add plotting of individual MIEZE curves and fit taking error bars into account.

miezelib.py
@@ -1,12 +1,14 @@
+import os
 import re
 import sys
 import copy
+import weakref
 from itertools import izip, cycle, groupby
 
-from numpy import array, linspace, exp, sqrt, sum, power, ceil
+from numpy import array, arange, linspace, exp, sin, sqrt, sum, power, ceil, pi
 import numpy as np
 
-from scipy.optimize import leastsq
+from scipy.odr import RealData, Model, ODR
 import scipy as sp
 
 try:
@@ -14,7 +16,7 @@ try:
 except ImportError:
     import pylab as pl
 
-__all__ = ['Data', 'np', 'pl', 'ml_debug']
+__all__ = ['Data', 'np', 'pl', 'ml_debug', 'ml_setdatadir']
 
 # -- helper for calculating tau_MIEZE ------------------------------------------
 
@@ -47,6 +49,12 @@ def dprint(*args):
 
 # -- helper for reading data files ---------------------------------------------
 
+_datadir = '.' 
+
+def ml_setdatadir(dir):
+    global _datadir
+    _datadir = dir
+
 def try_float(f):
     try:
         return float(f)
@@ -78,6 +86,7 @@ class Data(object):
         self.mess = {}
         self.norm = {}
         self.back = {}
+        self.collections = weakref.WeakKeyDictionary()
         dprint('created new data object:', name)
 
     def __repr__(self):
@@ -87,10 +96,15 @@ class Data(object):
         new = copy.copy(self)
         new.name = newname
 
-    def read_file(self, file, dct, variable=True, varvalue=None,
+    def read_file(self, file, dct, variable=True, varvalue=None, vals=None,
                   onlyval=None, ipars=None, cat=None):
         if cat is None and ipars:
             cat = ipars[0]
+        if isinstance(file, int):
+            file = 'mieze_%08d' % file
+        file = os.path.join(_datadir, file)
+        if os.path.isdir(file):
+            file = os.path.join(file, 'summary')
         data, fcomments = read_datafile(file)
         for fcomment in fcomments:
             if fcomment.endswith('file'):
@@ -105,7 +119,8 @@ class Data(object):
                    'different variables in data files'
         for row in data:
             point = dict(zip(fields, row))
-            if 'tau' not in point:
+            point['summaryfile'] = file
+            if 'tau' not in point or point['tau'] == '-':
                 assert ipars, \
                        'lam and L_s not given, but tau not in data file'
                 point['tau'] = _mieze_time(ipars[0]*1e-10, ipars[1]*1e-3,
@@ -115,7 +130,11 @@ class Data(object):
                 if varvalue is not None:
                     ddct = dct.setdefault(varvalue, {})
                 else:
-                    ddct = dct.setdefault(point[self.variable+'_set'], {})
+                    vv = point[self.variable+'_set']
+                    if vals is not None:
+                        if vv not in vals:
+                            continue
+                    ddct = dct.setdefault(vv, {})
                 ddct[point['tau']] = point
             else:
                 if onlyval is not None:
@@ -123,15 +142,24 @@ class Data(object):
                         continue
                 dct[point['tau']] = point
 
-    def read_data(self, file, varvalue=None, onlyval=None, ipars=None, cat=None):
-        self.read_file(file, self.mess, True, varvalue, onlyval, ipars, cat)
+    def read_data(self, file, varvalue=None, vals=None, onlyval=None, ipars=None, cat=None):
+        self.read_file(file, self.mess, True, varvalue, vals, onlyval, ipars, cat)
         dprint('read file', file, 'as data')
+
     def read_norm(self, file, onlyval=None, ipars=None, cat=None):
-        self.read_file(file, self.norm, False, None, onlyval, ipars, cat)
+        self.read_file(file, self.norm, False, None, None, onlyval, ipars, cat)
         dprint('read file', file, 'as normalization')
+
     def read_back(self, file, onlyval=None, ipars=None, cat=None):
-        self.read_file(file, self.back, False, None, onlyval, ipars, cat)
+        self.read_file(file, self.back, False, None, None, onlyval, ipars, cat)
         dprint('read file', file, 'as background')
+
+    def _filenames(self, meas, graph, bkgrd):
+        fn = lambda p: os.path.join(os.path.dirname(p['summaryfile']),
+                                    '%05d' % int(p['in file']))
+        return (fn(meas),
+                graph and fn(graph) or '',
+                bkgrd and fn(bkgrd) or '')
 
     def process(self, ycol='C', dpoints=ALL):
         if dpoints is ALL:
@@ -142,8 +170,10 @@ class Data(object):
             for x, p in self.mess[dpoint].items():
                 graph = self.norm.get(x)
                 bkgrd = self.back.get(x)
-                # correction factor for time-dependent values
+                # correction factor for time-dependent measurement values
                 cf = bkgrd and p['preset']/bkgrd['preset'] or 1
+                # and for graphite values
+                cfg = (graph and bkgrd) and graph['preset']/bkgrd['preset'] or 1
                 if ycol == 'sum':
                     c, m = p['countsum'], p['monitor']
                     y = c/m
@@ -152,19 +182,23 @@ class Data(object):
                         cb = bkgrd['countsum']
                         mb = bkgrd['monitor']
                         y -= cb/mb
-                        dy += yless*(1/sqrt(cb) + 1/sqrt(mb))
+                        dy += y*(1/sqrt(cb) + 1/sqrt(mb))
                 elif ycol == 'A':
-                    y = p['A'] / p['preset'] # XXX fehler
-                    dy = p.get('delta A', 0) / p['preset']
+                    y = p['A']
+                    dy = p.get('delta A', 0)
                     if bkgrd:
                         y -= bkgrd['A'] * cf
                         dy += bkgrd['delta A'] * cf
+                    y /= p['preset']
+                    dy /= p['preset']
                 elif ycol == 'B':
-                    y = p['B'] / p['preset']
-                    dy = p.get('delta B', 0) / p['preset']
+                    y = p['B']
+                    dy = p.get('delta B', 0)
                     if bkgrd:
                         y -= bkgrd['B'] * cf
                         dy += bkgrd.get('delta B', 0) * cf
+                    y /= p['preset']
+                    dy /= p['preset']
                 elif ycol == 'C':
                     a, b = p['A'], p['B']
                     dc = p.get('delta C', 0)
@@ -172,21 +206,23 @@ class Data(object):
                     if bkgrd:
                         a -= bkgrd['A'] * cf
                         b -= bkgrd['B'] * cf
-                        dc += bkgrd.get('delta C', 0)
                         c = a/b
+                        dc = (p['delta A'] + bkgrd['delta A']*cf +
+                              c*(p['delta B'] + bkgrd['delta B']*cf))/(p['B'] - bkgrd['B']*cf)
                     if graph:
                         ga, gb = graph['A'], graph['B']
                         gdc = graph['delta C']
                         gc = ga/gb
                         if bkgrd:
-                            ga -= bkgrd['A'] / bkgrd['preset'] * graph['preset']
-                            gb -= bkgrd['B'] / bkgrd['preset'] * graph['preset']
-                            gdc += bkgrd.get('delta C', 0)
+                            ga -= bkgrd['A'] * cfg
+                            gb -= bkgrd['B'] * cfg
                             gc = ga/gb
+                            gdc = (graph['delta A'] + bkgrd['delta A']*cfg +
+                                   gc*(graph['delta B'] + bkgrd['delta B']*cfg))/(graph['B'] - bkgrd['B']*cfg)
                         dc = (c/gc)*(dc/c + gdc/gc)
                         c = c/gc
                     y, dy = c, dc
-                xydy.append((x, p['cat'], y, dy))
+                xydy.append((x, p['cat'], y, dy, self._filenames(p, graph, bkgrd)))
             xydy.sort()
             data = [xydy, '%s %s' % (dpoint, self.unit)]
             curves.append(data)
@@ -199,67 +235,84 @@ class Data(object):
             num = num.replace('e', '\\cdot 10^{') + '}'
         return num
 
-    def model(self, v, x, y):
-        return v[0]*exp(-abs(v[1])*x) - y
+    def model(self, v, x):
+        return v[0]*exp(-abs(v[1])*x)
 
     MARKERS = ['o', '^', 's', 'D', 'v']
 
-    def plot(self, fit=False, color='r', ylabel=None, bycat=False,
-             subplots=False, **kwds):
+    def plot(self, fig, fit=False, color='r', ylabel=None, bycat=False,
+             subplots=False, lines=False, **kwds):
         curves = self.process(**kwds)
 
         if subplots:
-            pl.subplots_adjust(wspace=0.3)
+            fig.subplots_adjust(wspace=0.3)
             nrows = ceil(len(curves)/2.)
 
         for j, (xydy, label) in enumerate(curves):
             # setup plot
             if subplots:
-                pl.subplot(nrows, 2, j+1)
-                pl.title(label)
-            pl.xlabel('$\\tau_{MIEZE}$ / ps')
-            if ylabel is not None:
-                pl.ylabel(ylabel)
+                ax = fig.add_subplot(nrows, 2, j+1)
+                ax.set_title(label)
             else:
-                pl.ylabel(kwds.get('ycol', 'C') + (self.norm and ' (norm)' or ''))
+                ax = fig.gca()
+            ax.set_xlabel('$\\tau_{MIEZE}$ / ps')
+            if ylabel is not None:
+                ax.set_ylabel(ylabel)
+            else:
+                ax.set_ylabel(kwds.get('ycol', 'C') +
+                              (self.norm and ' (norm)' or ''))
 
             # plot data
-            x, _, y, dy = map(array, zip(*xydy))
+            x, _, y, dy, sf = map(array, zip(*xydy))
+            kwds = {'picker': 5, 'ls': lines and 'solid' or ''}
+            if color is not None:
+                kwds['color'] = color
             if not bycat:
-                pl.errorbar(x, y, dy, label=label, color=color, marker='o', ls='')
+                coll = ax.errorbar(x, y, dy, label=label, marker='o', **kwds)
+                self.collections[coll[0]] = sf
             else:
                 # sort by category
                 xydy.sort(key=lambda v: v[1])
                 for catmarker, (catname, catxydy) in \
                         izip(cycle(self.MARKERS), groupby(xydy, lambda v: v[1])):
-                    cx, _, cy, cdy = map(array, zip(*catxydy))
+                    cx, _, cy, cdy, csf = map(array, zip(*catxydy))
                     catlabel = '%s %s' % (label, catname)
-                    pl.errorbar(cx, cy, cdy, label=catlabel,
-                                color=color, marker=catmarker, ls='')
-            if fit:
-                out = leastsq(self.model, x0=[1, 0], args=(x, y),
-                              maxfev=1200, full_output=True)
-                print 'fit message:', out[-2]
-                if 1 <= out[-1] <= 4:
-                    vfit = out[0]
-                    # chi2 is sum of (square of deviation / ideal value)
-                    chi2 = sum(power(self.model(vfit, x, y), 2) /
-                               self.model(vfit, x, 0))
-                    # chi2/ndf; ndf is (# points - # fit parameters)
-                    chi2norm = chi2 / (len(x) - 2)
-                    fx = linspace(x[0], x[-1], 1000)
-                    fy = self.model(vfit, fx, 0)
-                    pl.plot(fx, fy, 'b-', label='exp. fit')
-                    gamma = self.format_num_latex(abs(vfit[1]), 2)
-                    chi2norm = self.format_num_latex(chi2norm, 2)
-                    text = (r'$\Gamma = %s\,\mathrm{ps}^{-1}$' '\n'
-                            r'$\chi^2/\mathrm{ndf} = %s$'
-                            % (gamma, chi2norm))
-                    pl.text(0.03, 0.03, text, size='x-large',
-                            transform=pl.gca().transAxes)
-            pl.gca().set_ylim(ymin=0)
+                    coll = ax.errorbar(cx, cy, cdy, label=catlabel,
+                                       marker=catmarker, **kwds)
+                    self.collections[coll[0]] = csf
 
-    def key_release(self, event):
+            if fit:
+                dat = RealData(x, y, sy=dy)
+                odr = ODR(dat, Model(self.model), beta0=[1, 0],
+                          ifixx=array([0]*len(x)))  # fixed X values
+                out = odr.run()
+                vfit = list(out.beta)
+                errors = list(out.sd_beta)
+                if 1 <= out.info <= 3:
+                    ndf = len(x) - 2
+                    # chi2 is sum of (square of deviation / ideal value)
+                    chi2 = sum(power(self.model(vfit, x) - y, 2) /
+                               self.model(vfit, x))
+                    # chi2/ndf; ndf is (# points - # fit parameters)
+                    chi2norm = chi2/ndf
+                    fx = linspace(x[0], x[-1], 1000)
+                    fy = self.model(vfit, fx)
+                    ax.plot(fx, fy, 'm-', label='exp. fit')
+                    gamma = abs(vfit[1]) * 1000 # in 1/ns
+                    gamma_s = self.format_num_latex(gamma, 2)
+                    gamerr_s = self.format_num_latex(errors[1]*1000, 2)
+                    gamma_muev_s = self.format_num_latex(gamma * 0.657, 2)
+                    gamerr_muev_s = self.format_num_latex(errors[1] * 657, 2)
+                    chi2norm_s = self.format_num_latex(chi2norm, 2)
+                    text = (r'$\Gamma = %s\,\mathrm{ns}^{-1} \hat{=}\, '
+                            r'%s\,\mathrm{\mu eV}$' '\n'
+                            r'$\chi^2/\mathrm{ndf} = %s$'
+                            % (gamma_s, gamma_muev_s, chi2norm_s))
+                    ax.text(0.03, 0.03, text, size='x-large',
+                            transform=ax.transAxes)
+            ax.set_ylim(ymin=0)
+
+    def on_key_release(self, event):
         if event.key == 'q':
             try:
                 event.canvas.manager.destroy()
@@ -278,17 +331,100 @@ class Data(object):
             ax.set_ylim(ylim)
             ax.figure.canvas.draw()
 
+    def on_pick(self, event):
+        npoint = event.ind[0]
+        collection = event.artist
+        if collection not in self.collections:
+            return
+        filenames = self.collections[collection][npoint]
+        self.plotmieze(filenames)
+
     def plotfig(self, filename=None, title=None, legend=True, **kwds):
-        pl.figure()
-        self.plot(**kwds)
+        self.fig = pl.figure()
+        self.plot(self.fig, **kwds)
         if title is not None:
-            pl.suptitle(title, size='x-large')
+            self.fig.suptitle(title, size='x-large')
         else:
-            pl.suptitle(self.name + ' - ' + kwds.get('ycol', 'C'), size='x-large')
+            self.fig.suptitle(self.name, size='x-large')
         if legend:
-            pl.legend(loc=(1,0))
-            pl.subplots_adjust(right=0.8)
-        pl.connect('key_release_event', self.key_release)
+            self.fig.gca().legend(loc=(1,0))
+            self.fig.subplots_adjust(right=0.8)
+        self.fig.canvas.mpl_connect('key_release_event', self.on_key_release)
+        self.fig.canvas.mpl_connect('pick_event', self.on_pick)
         if filename is not None:
-            pl.savefig(filename)
+            self.fig.savefig(filename)
             print 'Wrote', filename
+
+    pm_re = re.compile(r'([0-9e.]+)\s+\+/-\s+([0-9e.]+)')
+            
+    def plotmieze(self, filenames):
+        """Plot single MIEZE curves."""
+        
+        def fileinfo(filename):
+            points = []
+            for line in open(filename):
+                line = line.strip()
+                if not line: continue
+                if not line.startswith('#'):
+                    points.append(int(line))
+                    continue
+                line = line[2:]
+                if line.startswith('t/s:'):
+                    preset = int(line[5:])
+                elif line.startswith('sum:'):
+                    countsum = int(line[5:])
+                elif line.startswith('mon:'):
+                    monitor = int(line[5:])
+                elif line.startswith('A:'):
+                    A, dA = map(float, self.pm_re.match(line[5:]).groups())
+                elif line.startswith('B:'):
+                    B, dB = map(float, self.pm_re.match(line[5:]).groups())
+                elif line.startswith('C:'):
+                    C, dC = map(float, self.pm_re.match(line[5:]).groups())
+                elif line.startswith('phi:'):
+                    phi, dphi = map(float, self.pm_re.match(line[5:]).groups())
+            points = array(points)
+            return points, preset, countsum, monitor, A, dA, B, dB, C, dC, phi, dphi
+
+        def mz(x, A, B, phi):
+            return B + A*sin(pi/4*x + phi)
+
+        def plotinfo(filename, name, ax):
+            info = fileinfo(filename)
+            points, preset, countsum, monitor, A, dA, B, dB, C, dC, phi, dphi = info
+       
+            ax.set_title('%s: %s\n'
+                         r'$C = %.2f \pm %.2f$, $A = %.2f \pm %.2f$, $B = %.2f \pm %.2f$, '
+                         r'$\Sigma = %s$, $t = %s$' %
+                         (name, filename, C, dC, A, dA, B, dB, countsum, preset))
+            ax.set_ylabel('counts')
+            ax.errorbar(arange(1, 17), points, sqrt(points), fmt='ro')
+
+            xs = arange(0, 16, 0.1)
+            ys = mz(xs, A, B, phi)
+            ax.plot(xs, ys, 'b-')
+            ax.set_ylim(ymin=0)
+
+        fig = pl.figure()
+        fig.canvas.mpl_connect('key_release_event', self.on_key_release)
+        if filenames[1] and filenames[2]:
+            ax = fig.add_subplot(311)
+            plotinfo(filenames[0], 'Measurement', ax)
+            ax = fig.add_subplot(312)
+            plotinfo(filenames[1], 'Normalization', ax)
+            ax = fig.add_subplot(313)
+            plotinfo(filenames[2], 'Background', ax)
+        elif filenames[1]:
+            ax = fig.add_subplot(211)
+            plotinfo(filenames[0], 'Measurement', ax)
+            ax = fig.add_subplot(212)
+            plotinfo(filenames[1], 'Normalization', ax)
+        elif filenames[2]:
+            ax = fig.add_subplot(211)
+            plotinfo(filenames[0], 'Measurement', ax)
+            ax = fig.add_subplot(212)
+            plotinfo(filenames[2], 'Background', ax)
+        else:
+            ax = fig.gca()
+            plotinfo(filenames[0], 'Measurement', ax)
+        fig.subplots_adjust(hspace=0.3, bottom=0.05)